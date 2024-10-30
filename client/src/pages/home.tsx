3c6a4b3620850d00ea0ab04ee6e9b6f84c7be3cf
Matt Linder <linder2015@outlook.com>
2024-10-30 15:44:39 -0400

    updated exercise list and added images

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,5 +1,20 @@
-import styles from "./home.module.css";
+// import styles from "./home.module.css";
+
+// pages:
+// - home
+//   - hero
+//   - features
+//   - about / mission
+//   - reviews?
+// - content
+// - legal
+// - privacy
 
 export default function Home() {
-  return <section className={styles.section}></section>;
+  return (
+    <>
+      <header></header>
+      <main></main>
+    </>
+  );
 }
