a6ffdefe142d18f2dacdda205df634620e0bff25
Matt Linder <linder2015@outlook.com>
2024-11-02 20:40:59 -0400

    changed exercise list

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,4 +1,6 @@
-// import styles from "./home.module.css";
+import styles from "./home.module.css";
+import img from "../assets/deadlift.jpg";
+import { FaArrowDown, FaList } from "react-icons/fa";
 
 // pages:
 // - home
@@ -13,8 +15,39 @@
 export default function Home() {
   return (
     <>
-      <header></header>
-      <main></main>
+      <header className={styles.header}>
+        <nav>
+          <span>Vincera</span>
+          <div>
+            <span>Features</span>
+            <span>About</span>
+            <span>Contact</span>
+            <span>Legal</span>
+          </div>
+        </nav>
+      </header>
+      <main>
+        <section className={styles.hero}>
+          <img src={img} alt="" className={styles["hero-img"]} />
+          <div className={styles["hero-text-container"]}>
+            <h1>Vincera</h1>
+            <h3>Conquer the Gym</h3>
+            <div className={styles["hero-btn-container"]}>
+              <button className={styles["btn-features"]}>
+                <FaList />
+                <span>Features</span>
+              </button>
+              <button className={styles["btn-download"]}>
+                <FaArrowDown />
+                <span>Download</span>
+              </button>
+            </div>
+          </div>
+        </section>
+        <section></section>
+        <section></section>
+        <section></section>
+      </main>
     </>
   );
 }
